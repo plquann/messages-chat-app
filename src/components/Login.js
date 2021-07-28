d34e1dc31619a6e60d4d13447e5b22706642de35
QuanKhs <quanphamluong@gmail.com>
2021-07-28 09:34:40 +0700

    add context auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+import 'firebase/app';
+
+import { auth } from '../firebase';
+import firebase from 'firebase/app';
 
 const Login = () => {
     return (
@@ -7,10 +11,16 @@ const Login = () => {
             <div id="login-card">
                 <h2>Welcome to Messages Chat App</h2>
 
-                <div className="login-btn google">
+                <div
+                    className="login-btn google"
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                >
                     <GoogleOutlined className="google-icon" />Sign In with Google
                 </div>
-                <div className="login-btn facebook">
+                <div
+                    className="login-btn facebook"
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
+                >
                     <FacebookOutlined className="facebook-icon" />Sign In with Facebook
                 </div>
             </div>
